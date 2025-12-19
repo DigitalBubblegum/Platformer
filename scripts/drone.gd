ed4082c5c42dc9e7c0f0e6649a43b7d64fd2a283
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2025-12-19 21:54:46 +0530

    added a drone with player detectiun

---
+++ scripts/drone.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+@export var droneHealth:int = 3
+var player: CharacterBody2D
+var directions = [Vector2i(-1,-1),Vector2i(0,-1),Vector2i(1,-1),Vector2i(1,0),Vector2i(1,1),Vector2i(0,-1),Vector2i(-1,1),Vector2i(-1,0)]
+var currentDir= Vector2i(-1,-1)
+@export var droneSpeed = 20
+var isPlayerDetected:bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	if player and isPlayerDetected:
+		var dir= (player.position - position).normalized()
+		velocity = dir * droneSpeed
+		move_and_slide()
+	else:
+		droneMovement(delta)
+
+func droneMovement(delta:float):
+	position += droneSpeed * currentDir * delta
+
+func _on_move_random_timer_timeout() -> void:
+	currentDir = directions.pick_random()
+
+func droneDamage():
+	droneHealth=-1
+
+func droneCurrentHealth():
+	return droneHealth
+
+func setPlayerDetected():
+	isPlayerDetected=!isPlayerDetected
+	return isPlayerDetected
+	
+func _on_detection_area_body_entered(playerbody: CharacterBody2D) -> void:
+	player = playerbody
+	setPlayerDetected()
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	setPlayerDetected()
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 var bullet_scene = preload("res://scenes/bullets/bullet.tscn")
-
+var drone_scene = preload("res://scenes/entities/drone.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,5 +13,9 @@ func _process(_delta: float) -> void:
 
 func _on_player_shoot(pos: Vector2, dir: Vector2) -> void:
 	var bullet = bullet_scene.instantiate() as Area2D
+	#var drone = drone_scene.instantiate() as Area2D
 	$Bullets.add_child(bullet)
 	bullet.setup(pos,dir)
+	#if(drone.droneCurrentHealth()):
+		#print(drone.droneCurrentHealth())
+	#if()
